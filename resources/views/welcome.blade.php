3b4384d82fb025de3b967a11bcb9678ddea69e98
JoaoVitordSRodrigues <joao.rodrigues328@tec.sp.gov.br>
2023-03-03 17:01:39 -0300

    project_laravelBootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Bootstrap w/ Vite</title>
+
+        @vite([
+            'resources/css/app.css', 
+            'resources/js/app.js',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/js/bootstrap.bundle.js'   
+        ])
+    </head>
+    <body>
+        <div class="container py-4 px-3 mx-auto">
+            <h1>Hello,  Bootstrap and Vite!</h1>
+            <button class="btn btn-primary">Primary button</button>
+        <div>
+    </body>
+</html>
